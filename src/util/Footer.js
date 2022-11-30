3ed55753dca0991d27032cdc98bc5230657f24a5
HemanthSingirikonda <hemanthsingirikonda050@gmail.com>
2022-12-01 00:54:36 +0530

    Adding Project Code to upload on Github

---
+++ src/util/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<h4>
+				Built With 💛 by &nbsp;<a href='https://github.com/HemanthSingirikonda'>Hemanth Singirikonda</a>
+			</h4>
+		</footer>
+	);
+};
+
+export default Footer;
